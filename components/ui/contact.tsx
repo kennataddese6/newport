c7cd9056ef09ac68077a3cde0cdec3e4b7f66ac2
kennataddese6 <kennataddese6@gmail.com>
2025-07-21 18:02:48 +0300

    add contact holder image

--- components/ui/contact.tsx
+++ components/ui/contact.tsx
@@ -1,34 +1,46 @@
+import Image from "next/image"
+
 const Contact = () => {
   return (
-    <div className="relative z-10 w-full">
-      <h1 className="text-3xl font-semibold text-center mt-24 mb-12">
+    <div className="w-full">
+      <h1 className="text-3xl font-semibold text-center mt-24 mb-12 py-48">
         Talk to me
       </h1>
-      <form className="w-full md:w-[40%]  border-2 border-neutral-800 mx-auto px-8 py-16 rounded-lg">
-        <input
-          type="text"
-          placeholder="Your Name"
-          className="border-2 border-neutral-800 p-3 w-full rounded-md"
+      <div className="relative  z-10 w-full">
+        <Image
+          src={"/contactholder.png"}
+          width={600}
+          height={600}
+          alt="Contact"
+          className="w-[42rem] absolute left-1/2 -translate-x-1/2 bottom-0 -z-10"
         />
-        <br />
-        <br />
-        <input
-          type="text"
-          placeholder="Your Email"
-          className=" border-2 border-neutral-800 p-3 w-full rounded-md"
-        />
-        <br />
-        <br />
-        <textarea
-          className="w-full border-2 border-neutral-800 p-3"
-          placeholder="Your Message"
-        ></textarea>
-        <br />
-        <br />
-        <button className="border-2 border-neutral-800 w-full p-3 rounded-md">
-          Send
-        </button>
-      </form>
+
+        <form className="w-full md:w-[32%] bg-black/20 backdrop-blur-lg  border-2 border-neutral-800 mx-auto px-8 py-14 rounded-lg">
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="border-2 border-neutral-800/50 p-3 w-full rounded-md"
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Your Email"
+            className=" border-2 border-neutral-800/50 p-3 w-full rounded-md"
+          />
+          <br />
+          <br />
+          <textarea
+            className="w-full border-2 border-neutral-800/50 p-3"
+            placeholder="Your Message"
+          ></textarea>
+          <br />
+          <br />
+          <button className="border-2 border-neutral-800/50 w-full p-3 rounded-md">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
