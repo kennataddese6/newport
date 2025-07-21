e00f9f5328dee7fe66fc082bcf875dd353c12ae7
kennataddese6 <kennataddese6@gmail.com>
2025-07-21 17:02:11 +0300

    add contact form

--- components/ui/contact.tsx
+++ components/ui/contact.tsx
@@ -1,7 +1,34 @@
 const Contact = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-semibold text-center mt-24 ">Talk to me</h1>
+    <div className="relative z-10 w-full">
+      <h1 className="text-3xl font-semibold text-center mt-24 mb-12">
+        Talk to me
+      </h1>
+      <form className="w-full md:w-[40%]  border-2 border-neutral-800 mx-auto px-8 py-16 rounded-lg">
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="border-2 border-neutral-800 p-3 w-full rounded-md"
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Your Email"
+          className=" border-2 border-neutral-800 p-3 w-full rounded-md"
+        />
+        <br />
+        <br />
+        <textarea
+          className="w-full border-2 border-neutral-800 p-3"
+          placeholder="Your Message"
+        ></textarea>
+        <br />
+        <br />
+        <button className="border-2 border-neutral-800 w-full p-3 rounded-md">
+          Send
+        </button>
+      </form>
     </div>
   )
 }
