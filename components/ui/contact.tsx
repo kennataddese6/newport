9ab0170436b51c4f2312d699050482316093136d
kennataddese6 <kennataddese6@gmail.com>
2025-07-23 17:57:20 +0300

    add send email functionality

--- components/ui/contact.tsx
+++ components/ui/contact.tsx
@@ -1,6 +1,35 @@
+import emailjs from "@emailjs/browser"
 import Image from "next/image"
+import { useRef } from "react"
+import { toast } from "sonner"
 import { Input } from "../ui/input"
 const Contact = () => {
+  const form: any = useRef(null)
+  const sendEmail = (formData: FormData) => {
+    try {
+      emailjs.send(
+        process.env.NEXT_PUBLIC_SERVICE_KEY || "",
+        process.env.NEXT_PUBLIC_TEMPLATEID || "",
+        {
+          user_name: formData.get("user_name"),
+          user_email: formData.get("user_email"),
+          message: formData.get("message"),
+          email: process.env.NEXT_PUBLIC_EMAIL,
+        },
+        {
+          publicKey: process.env.NEXT_PUBLIC_KEY,
+        },
+      )
+      form.current?.reset()
+      toast.success("Message recieved", {
+        description: "I will reply to you very soon.",
+        style: { backgroundColor: "black" },
+      })
+    } catch (error: any) {
+      toast.error("error")
+    }
+  }
+
   return (
     <div className="w-full" id="contact">
       <h1 className="text-3xl font-semibold text-center mt-24 mb-12 md:pb-48">
@@ -30,19 +59,27 @@ const Contact = () => {
           className="w-[24rem] -rotate-z-12 absolute -right-32 -translate-x-1/2 bottom-36 -z-10"
         />
 
-        <form className="w-[96%] md:w-[32%] bg-black/20 backdrop-blur-lg  border-2 border-neutral-800 mx-auto px-8 py-14 rounded-lg">
-          <Input id="firstname" placeholder="Your Name" type="text" />
+        <form
+          action={sendEmail}
+          ref={form}
+          className="w-[96%] md:w-[32%] bg-black/20 backdrop-blur-lg  border-2 border-neutral-800 mx-auto px-8 py-14 rounded-lg"
+        >
+          <Input id="firstname" placeholder="Your Name" type="text" required />
           <br />
-          <Input id="firstname" placeholder="Your Email" type="text" />
+          <Input id="firstname" placeholder="Your Email" type="text" required />
           <br />
 
           <textarea
-            className="w-full border-2 border-neutral-800/50 p-3 "
-            placeholder="Your Message"
+            required
+            className="w-full border-2 border-neutral-800/50 p-3"
+            placeholder="What do you want to say?"
           ></textarea>
           <br />
           <br />
-          <button className="border-2 cursor-pointer border-neutral-800/50 hover:bg-black/40 w-full p-3 rounded-md">
+          <button
+            type="submit"
+            className="border-2 cursor-pointer border-neutral-800/50 hover:bg-black/40 w-full p-3 rounded-md"
+          >
             Send
           </button>
         </form>
