f39251156d83e9bea04de3201ae4878b81d614b4
kennataddese6 <kennataddese6@gmail.com>
2025-07-25 14:32:03 +0300

    add anim on holla image

--- components/ui/holla.tsx
+++ components/ui/holla.tsx
@@ -5,13 +5,24 @@ const Holla = () => {
   return (
     <div className="relative z-0 w-full">
       <h1 className="text-3xl font-semibold text-center mt-24 ">Holla at me</h1>
-      <Image
-        src={"/phonecall.png"}
-        alt="phone call image"
-        width={1800}
-        height={1800}
-        className="w-[28rem] cursor-pointer block mx-auto"
-      />
+      <motion.div
+        initial={{ scale: 0.8, rotate: -10, translateY: 180 }}
+        whileHover={{ scale: 1, rotate: 0, translateY: 0 }}
+        transition={{
+          duration: 0.7,
+          stiffness: 300,
+          damping: 20,
+          ease: "easeInOut",
+        }}
+      >
+        <Image
+          src={"/phonecall.png"}
+          alt="phone call image"
+          width={1800}
+          height={1800}
+          className="w-[28rem] cursor-pointer block mx-auto"
+        />
+      </motion.div>
       <motion.div className="text-white absolute top-10/12 left-[30%] font-semibold border w-fit py-1 px-3 rounded-full">
         +1 (530) 712-3425
       </motion.div>
